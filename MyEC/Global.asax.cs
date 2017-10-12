8f0cd3dc66ac2c68b0547edf6f05c1ce7292c455
MiyazakiRyuta <ryuta_miyazaki@ot.olmpus.co.jp>
2017-10-12 15:17:07 +0900

    基本構成構築

--- MyEC/Global.asax.cs
+++ MyEC/Global.asax.cs
@@ -14,5 +14,10 @@ namespace MyEC
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Application_Error(Object sender, EventArgs e)
+        {
+            var a = Server.GetLastError();
+        }
     }
 }
