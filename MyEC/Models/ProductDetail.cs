3ee8f311152bfc2b283f63a836a6241a141e86f0
MiyazakiRyuta <ryuta_miyazaki@ot.olmpus.co.jp>
2017-10-27 19:38:35 +0900

    詳細画面追加

--- MyEC/Models/ProductDetail.cs
+++ MyEC/Models/ProductDetail.cs
@@ -7,5 +7,9 @@ namespace MyEC.Models
 {
     public class ProductDetail : Base
     {
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductDetailDescription { get; set; }
     }
 }
